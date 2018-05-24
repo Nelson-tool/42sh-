533eb673e8daa4279aeedcbb47d34a46ce2253b2
alexis <alexis.auriac@epitech.eu>
2018-05-24 15:45:48 +0200

    Modified builtin alias so it works more like the one from tcsh

--- src/builtins/builtin_alias.c
+++ src/builtins/builtin_alias.c
@@ -10,25 +10,25 @@
 #include "my.h"
 #include "shell.h"
 
-void builtin_alias(shell_t *mysh, char **command)
+static void show_alias(alias_t *alias, const char *name)
 {
 	char *value;
 
+	if (alias == NULL)
+		return;
+	value = find_alias(alias, name);
+	if (value != NULL)
+		puts(value);
+}
+
+void builtin_alias(shell_t *mysh, char **command)
+{
 	if (command[1] == NULL) {
 		if (mysh->alias)
 			show_alias_tree(mysh->alias);
-	}
-	for (int i = 1 ; command[i] ; ++i) {
-		value = index(command[i], '=');
-		if (value != NULL) {
-			*value = '\0';
-			++value;
-			set_alias(mysh, command[i], value);
-		} else {
-			value = (mysh->alias) ? find_alias(mysh->alias, command[i]) : NULL;
-			if (value != NULL)
-				puts(value);
-		}
-	}
+	} else if (command[2] == NULL)
+		show_alias(mysh->alias, command[1]);
+	else
+		set_alias(mysh, command[1], command[2]);
 	mysh->exit_status = 0;
 }
